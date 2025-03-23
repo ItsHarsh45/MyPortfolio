108d9af3b7402be264ce52c98af855feb2468ac3
HarshKemali <86662582+ItsHarsh45@users.noreply.github.com>
2025-03-23 15:32:59 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
     {
       name: 'React',
       url: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg',
-      subtopics: ['Hooks', 'Tailwind CSS', 'Redux', 'Next.js'],
+      subtopics: ['Hooks', 'Tailwind CSS', 'Context/Redux', 'Next.js'],
       details: 'Proficient in building complex user interfaces with React.'
     },
     {
